ea1969c437120f48e0262f299ca71f8a92245d41
Felipe Botelho de Souza <botelho422@gmail.com>
2023-08-31 08:22:36 -0300

    habilitando cors corretamente

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask import request
 import json
 from controllers.botController import training_bot, get_response, create_bot, list_bots, list_intents
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def index():
